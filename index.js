ab1d91e7396d679d243a229c9d54bb333825476f
phamtu24 <42486467+phamtu24@users.noreply.github.com>
2019-08-22 16:02:04 +0700

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const Message = require('./models/message');
@@ -8,6 +9,7 @@ const Member = require('./models/member');
 const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 app.get('/', async (req, res) => {
